4f51946c79dc245ad3bb7db7a8931529f1ad3006
NielsTanghe1 <57326188+NielsTanghe1@users.noreply.github.com>
2025-05-10 15:48:45 +0200

    Setup

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,20 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+}) -> name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/contact', function () {
+    return view('contact');
+}) -> name('contact');
+
+Route::get('/FAQ', function () {
+    return view('FAQ');
+}) -> name('FAQ');
+
+Route::get('/news', function () {
+    return view('news');
+}) -> name('news');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
